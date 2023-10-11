ff53294a06edf3e7338eb38cac19a001d913f3f7
Tim Kuijpers <timkuijpers2002@outlook.com>
2023-10-11 23:04:29 +0200

    Revert "added comment for pipeline test"
    This reverts commit 96323cf6eabb42ac7dace9e02a92c3621f198e7c.

--- PatientAPI/Services/PatientService.cs
+++ PatientAPI/Services/PatientService.cs
@@ -158,7 +158,6 @@ namespace PatientAPI.Services
             });
         }
 
-        //Declare a patient as death
         public override async Task<DeclareDeceasedPatientResponse> DeclareDeceasedPatient(DeclareDeceasedPatientRequest request, ServerCallContext context)
         {
             if (request.Id <= 0 || request.DateOfDeath == null)
